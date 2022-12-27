f1b10397904d09d39ce400dce95e35243169a59c
Mahnoor Zainab <mahnoorzainab99@gmail.com>
2022-12-27 11:36:57 +0500

    first commit

---
+++ src/Components/Hello.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hello = () => {
+  return (
+    <>
+    
+    <div className=" bg-dark text-white text-center" style={{marginTop:"200px",}} >
+      <h1> Hello World</h1>
+    </div>
+
+
+
+     </>
+  )
+}
+
+export default Hello
